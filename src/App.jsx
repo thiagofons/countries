186a4021257b7ea9b5f33df30915280efeafabaa
Thiago Arruda <thiago_arruda@id.uff.br>
2023-01-27 21:00:13 -0300

    setup

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import Container from './components/layout/Container';
+import NavBar from './components/layout/NavBar';
+import Map from './components/layout/Map';
+
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Container>
+        <NavBar />
+        <Map />
+      </Container>
+    </div>
+  );
+}
+
+export default App;
+
+
---
+++ src/components/pages/Map.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import styles from "./Map.module.css";
+
+const Map = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default Map;
